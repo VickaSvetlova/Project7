948afe21afacb5147f1e346685f3f838cd9d1920
vickasvetlova <vickasvetlova@gmail.com>
2017-04-26 17:50:04 +0300

    init 2

--- Assets/Scenes/GameController.cs
+++ Assets/Scenes/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class GameController : MonoBehaviour
     private int[] Damagerange = { 2, 10, 15, 30 };
     private float _damageCount;
     private int numPoint;
+    private bool _isDamage;
+    [SerializeField]
+    private Text[] textAtribute;
+
 
     private void Update()
     {
@@ -19,10 +24,15 @@ public class GameController : MonoBehaviour
     }
     private void TestDamage()
     {
+        _isDamage = false;
         for (int i = 0; i < PyramidTypeWound.Length; i++)
         {
+            if (_isDamage)
+            {
+                break;
+            }
 
-            if (RandomNum() >= Damagerange[i])
+            if (RandomNum() > Damagerange[i])
             {
                 for (int j = 0; j < Damagerange.Length; j++)
                 {
@@ -30,17 +40,22 @@ public class GameController : MonoBehaviour
                     {
                         PyramidLife[i, j] = 0;
                         //Debug.Log("You get a " + PyramidTypeWound[i]);
-                        break;                     
+                        _isDamage = true;
+                        break;
+                        
                     }
                 }
             }
+           
+
         }
         PyramidDebug();
+
     }
 
     private int RandomNum()
     {
-        int random = 2;//Random.Range(1, 1);
+        int random = 15;//Random.Range(1, 1);
         return random;
     }
 
@@ -56,7 +71,8 @@ public class GameController : MonoBehaviour
                     numPoint += 1;
                 }
             }
-            Debug.Log(PyramidTypeWound[i] + " " + numPoint);
+            //Debug.Log(PyramidTypeWound[i] + " " + numPoint);
+            textAtribute[i].text=PyramidTypeWound[i] + " " + numPoint;
         }
     }
 }
